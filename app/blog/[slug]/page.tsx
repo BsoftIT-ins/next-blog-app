c64f5c77bb4399cb5d7dc12f1fa1e4a4774bc251
Azizul Islam <bsoftitinfo@gmail.com>
2024-06-04 08:28:33 +0600

    Finished project

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,5 +1,7 @@
 import { fullBlog } from "@/app/lib/interface";
-import { client } from "@/app/lib/sanity";
+import { client, urlFor } from "@/app/lib/sanity";
+import { PortableText } from "next-sanity";
+import Image from "next/image";
 
 async function getData(slug:string){
     const query =`
@@ -16,7 +18,27 @@ async function getData(slug:string){
 }
 
 
-export default async function BlogArticle({params} : {params:{slug:string}}){
-    const data: fullBlog = await getData(params.slug)
-    return <h1>{params.slug}</h1>
+export default async function BlogArticle({
+    params,
+} : {
+    params:{slug:string};
+}){
+    const data: fullBlog = await getData(params.slug);
+
+    console.log(data);
+
+    return (
+        <div>
+            <h1>
+                <span className="block text-base text-center text-primary font-semibold tracking-wide uppercase">Azizul's Blog</span>
+                <span className="mt-2 block text-3xl text-center leading-8 font-bold tracking-tight sm:text-4xl ">{data.title}</span>
+            </h1>
+            <Image src={urlFor(data.titleImage).url()} width={800} height={800} alt="Title Image" priority className="rounded-lg mt-8 border"/>
+            <div className="mt-16 prose prose-blue prose-lg dark:prose-invert prose-li:marker:text-primary prose-headings:text-left text-justify prose-a:text-primary ">
+            <PortableText value={data.content} />
+            </div>
+        </div>
+        
+
+    )
 }
